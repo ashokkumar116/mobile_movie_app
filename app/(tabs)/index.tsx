e062e0c65680037b8eaa07d8563e9c2cf3a1b7ba
ASHOK KUMAR P <ashokkumarpandian7@gmail.com>
2025-08-23 23:00:01 +0530

    Created app header ui

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,12 +1,23 @@
 import { Link } from "expo-router";
-import { Text, View } from "react-native";
+import { Image, ScrollView, Text, View } from "react-native";
+import { images } from "@/constants/images";
+import { icons } from "@/constants/icons";
 
 export default function Index() {
-  return (
-    <View className="flex-1 justify-center items-center" >
-      <Text className="text-secondary text-5xl">Welcome!</Text>
-    </View>
-
-
-  );
+    return (
+        <View className="flex-1 bg-primary">
+            <Image source={images.bg} className="absolute w-full z-0" />
+            <ScrollView className="flex-1 px-5">
+                <Image
+                    source={icons.logo}
+                    className="w-12 h-10 mt-20 mb-5 mx-auto"
+                    showsVerticalScrollIndicato={false}
+                    contentContainerStyle={{
+                        minHeight: "100%",
+                        paddingBottom: 10,
+                    }}
+                />
+            </ScrollView>
+        </View>
+    );
 }
