6a061756328d169cca5a8927627f94bba9af7258
ASHOK KUMAR P <ashokkumarpandian7@gmail.com>
2025-08-24 20:44:40 +0530

    Creted TMDB Account and used it to create api endpoint for fetching movies

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -8,13 +8,16 @@ export default function Index() {
 
     const router = useRouter();
 
+
     return (
         <View className="flex-1 bg-primary">
             <Image source={images.bg} className="absolute w-full z-0" />
             <ScrollView className="flex-1 px-5">
+
                 <Image
                     source={icons.logo}
                     className="w-12 h-10 mt-20 mb-5 mx-auto"
+                    // @ts-ignore
                     showsVerticalScrollIndicator={false}
                     contentContainerStyle={{
                         minHeight: "100%",
